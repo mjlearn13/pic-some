e7934d5e587171542079f5d450dc1f380f97c5ae
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2023-05-14 12:37:27 +0100

    added custom hook for the hover state

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,8 +1,10 @@
 import { useState, useContext } from 'react'
+import PropTypes from "prop-types"
 import { Context } from '../Context'
+import useHover from '../hooks/useHover'
 
 function CartItem({ item }) {
-  const [hovered, setHovered] = useState(false)
+  const [hovered, ref] = useHover()
   const { removeFromCart } = useContext(Context)
 
   const iconClassName = hovered ? 'ri-delete-bin-fill' : 'ri-delete-bin-line'
@@ -12,14 +14,20 @@ function CartItem({ item }) {
       <i
         className={iconClassName}
         onClick={() => removeFromCart(item.id)}
-        onMouseEnter={() => setHovered(true)}
-        onMouseLeave={() => setHovered(false)}
-      ></i>
+        ref={ref}
+      >
+      </i>
 
       <img src={item.url} width="130px" />
       <p>$5.99</p>
     </div>
   )
+}
+
+CartItem.propTypes = {
+    item: PropTypes.shape({
+        url: PropTypes.string.isRequired
+    })
 }
 
 export default CartItem
